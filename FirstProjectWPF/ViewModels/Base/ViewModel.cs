439a042e1e92556dd7e8c03756ec35d5be0ed2d0
microChelPchel <sachacok@yandex.ru>
2022-08-16 19:20:20 +0300

    Базовый класс модели-представления

---
+++ FirstProjectWPF/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace FirstProjectWPF.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnProperyChanged([CallerMemberName]string PropertName=null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertName));
+        }
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnProperyChanged(PropertyName);
+            return true;
+        }
+
+
+    }
+}
