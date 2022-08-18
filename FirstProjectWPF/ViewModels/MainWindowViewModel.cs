e80ecb1dcbab0a7f53e7e93989700f8abd7a9219
microChelPchel <sachacok@yandex.ru>
2022-08-18 10:08:59 +0300

    Статус и базовый класс команды

--- FirstProjectWPF/ViewModels/MainWindowViewModel.cs
+++ FirstProjectWPF/ViewModels/MainWindowViewModel.cs
@@ -19,5 +19,21 @@ namespace FirstProjectWPF.ViewModels
 
         #endregion
 
+
+        #region Status : string - status application
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов!";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+        #endregion
+
+
+
     }
 }
