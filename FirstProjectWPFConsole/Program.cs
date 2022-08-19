b1cf25237a50f561c1e413499ca7ec97753c5fe8
microChelPchel <sachacok@yandex.ru>
2022-08-19 15:23:49 +0300

    запрос к удаленному серверу для получения сырых данных

--- FirstProjectWPFConsole/Program.cs
+++ FirstProjectWPFConsole/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace FirstProjectWPFConsole
 {
     internal class Program
     {
+        private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //var web_client = new WebClient();
+
+            var client = new HttpClient();
+
+            //var items = new string[10];
+            //var last_item = items[^1];
+            //var prev_last_items=items[^2];
+
+            var response= client.GetAsync(data_url).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+
+            //3:15
+
+            Console.ReadLine();
+
         }
     }
 }
