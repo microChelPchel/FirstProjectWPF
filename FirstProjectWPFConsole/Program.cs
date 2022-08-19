0989f57c35171371f1574845016a498998e86a31
microChelPchel <sachacok@yandex.ru>
2022-08-19 17:18:54 +0300

    извлечение данных по датам

--- FirstProjectWPFConsole/Program.cs
+++ FirstProjectWPFConsole/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace FirstProjectWPFConsole
 {
     internal class Program
     {
         private const string data_url = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+
+        private static async Task<Stream> GetDataStream()
+        { 
+            var client = new HttpClient();
+            var responce = await client.GetAsync(data_url,HttpCompletionOption.ResponseHeadersRead);
+            return await responce.Content.ReadAsStreamAsync();
+        }
+
+        public static IEnumerable<string> GetDataLines()
+        {
+            using var data_stream = GetDataStream().Result;
+            using var data_reader = new StreamReader(data_stream);
+
+            while (!data_reader.EndOfStream)
+            { 
+                var line = data_reader.ReadLine();
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
+                yield return line;
+            }
+        }
+
+
+        private static DateTime[] GetDates() => GetDataLines().First().Split(',').Skip(4)
+            .Select(s=>DateTime.Parse(s, CultureInfo.InvariantCulture)).ToArray();
+
+
+
+
+        
+        
         static void Main(string[] args)
         {
             //var web_client = new WebClient();
@@ -17,11 +55,19 @@ namespace FirstProjectWPFConsole
             //var last_item = items[^1];
             //var prev_last_items=items[^2];
 
-            var response= client.GetAsync(data_url).Result;
-            var csv_str = response.Content.ReadAsStringAsync().Result;
+            //var response= client.GetAsync(data_url).Result;
+            //var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            //foreach (var data_line in GetDataLines())
+            //{
+            //    Console.WriteLine(data_line);
+            //}
+
+            var dates = GetDates();
+
+            Console.WriteLine(String.Join("\r\n",dates));
 
 
-            //3:15
 
             Console.ReadLine();
 
