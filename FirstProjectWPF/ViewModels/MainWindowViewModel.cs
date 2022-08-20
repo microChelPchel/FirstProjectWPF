7ac03eaee98eaecee82c17a3a5118571773d261e
microChelPchel <sachacok@yandex.ru>
2022-08-21 00:04:17 +0300

    Пример с управлением вкладками через модель-представление

--- FirstProjectWPF/ViewModels/MainWindowViewModel.cs
+++ FirstProjectWPF/ViewModels/MainWindowViewModel.cs
@@ -10,6 +10,21 @@ namespace FirstProjectWPF.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        #region
+        /// <summary>Номер выбранной вкладки</summary>
+        private int _SelectedPageIndex=0;
+
+        /// <summary>Номер выбранной вкладки</summary>
+        public int SelectedPageIndex
+        {
+            get =>_SelectedPageIndex;
+            set => Set(ref _SelectedPageIndex, value);
+        }
+        #endregion
+
+
+
+
         #region TestDataPoints
         private IEnumerable<DataPoint> _testDataPoints;
 
@@ -60,11 +75,23 @@ namespace FirstProjectWPF.ViewModels
         }
         #endregion
 
+        
+        public ICommand ChangeTabIngexCommand { get; }
+
+        public bool CanChangeTabIndexCommandExecute(object p) => _SelectedPageIndex >= 0;
+
+        public void OnChangeTabIndexCommandExecuted(object p)
+        {
+            if (p is null) return;
+            SelectedPageIndex += Convert.ToInt32(p);
+        }
+
         #endregion
 
         public MainWindowViewModel()
         {
             CloseApplicationCommand = new LambdaCommand(OnCloseApplicationCommandExecuted,CanCloseApplicationCommandExecute);
+            ChangeTabIngexCommand = new LambdaCommand(OnChangeTabIndexCommandExecuted, CanChangeTabIndexCommandExecute);
 
             var data_points = new List<DataPoint>((int)(360 / 0.1));
             for (var x = 0d; x <= 360; x += 0.1)
